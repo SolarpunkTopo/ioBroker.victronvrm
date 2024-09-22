e6e8fd35898a716236dd46292d67a0577e139979
Topo <balance240@gmail.com>
2024-09-22 13:47:12 +0000

    fix rootnode

--- main.js
+++ main.js
@@ -28,16 +28,12 @@ class VictronVrmAdapter extends utils.Adapter {
         // API-Key und andere Einstellungen aus den Konfigurationen lesen
         // Initialisiere die Konfiguration
 		const apiKey = this.config.apiKey;
-		const installationId = this.config.installationId;
-		const installationName = this.config.installationName;
 		const username = this.config.username;
-			
 		const password = this.config.password;
 		const interval = this.config.interval || 60;
 
+
 		this.username  			= username;
-		this.installationId		= installationId;
-		this.installationName	= installationName;
 		
 
         // Wenn Einstellungen fehlen, Fehlermeldung ausgeben
@@ -51,20 +47,20 @@ class VictronVrmAdapter extends utils.Adapter {
 	
 	try {
 				// API-Login und Abruf der API-Daten
+				
 				const { apiToken, idUser } = await this.getApiToken(username, password);
 				const  installations = await this.getInstallationId(apiToken, idUser);
-					
-				 if (installations.length > 0) {
-					// Wenn Installationen gefunden wurden, die erste Installation verwenden
-				const { id, name } = installations[0];	
-					 // Speichern der Installationen in der Adapterkonfiguration
-				this.updateConfig({ installations });		
-				// Speichern der API-Daten in der Adapterkonfiguration
-				this.updateConfig({ apiKey: apiToken, installationId: id, installationName: name });
-				this.installationIds = installations;
-				this.log.info('installationId ' + id + name);
-				this.log.info('Successfully fetched API token and installation ID.');
+				
 			
+			if (installations.length > 0) {
+					
+				// Speichern der API-Daten in der Adapterkonfiguration
+				this.apiKey				= apiKey;
+				this.idUser 			= idUser;
+				this.installationIds 	= installations;
+				this.updateConfig({ installations });
+				
+				this.log.info('Successfully fetched API token and installation ID.');
 		 }
 			
 			} catch (error) {
@@ -78,7 +74,7 @@ class VictronVrmAdapter extends utils.Adapter {
 	
 		
 		
-		this.setObjectNotExistsAsync('info.alive', {
+	this.setObjectNotExistsAsync('info.alive', {
     type: 'state',
     common: {
         name: 'Alive',
@@ -106,40 +102,7 @@ class VictronVrmAdapter extends utils.Adapter {
 
 
 async onMessage(obj) {
-    if (typeof obj === 'object' && obj.message) {
-        switch (obj.command) {
-            case 'fetchApiDetails':
-                if (obj.message.username && obj.message.password) {
-                    this.log.info('Message erfolgreich empfangen..');
-					this.FetchApiDetails(obj.message.username, obj.message.password)
-                        .then(result => {
-                            if (obj.callback) {
-                                // Bei Erfolg senden wir eine Rückmeldung mit dem Ergebnis zurück
-                                
-								this.sendTo(obj.from, obj.command, { success: true, result: result }, obj.callback);
-                            }
-                        })
-                        .catch(error => {
-                            if (obj.callback) {
-                                // Bei Fehler ebenfalls eine Rückmeldung
-                                this.sendTo(obj.from, obj.command, { success: false, error: error.message }, obj.callback);
-                            }
-                        });
-                } else {
-                    this.log.warn('Username or password is missing');
-                    if (obj.callback) {
-                        this.sendTo(obj.from, obj.command, { success: false, error: 'Username or password missing' }, obj.callback);
-                    }
-                }
-                break;
-            default:
-                this.log.warn('Unknown command: ' + obj.command);
-                if (obj.callback) {
-                    this.sendTo(obj.from, obj.command, { success: false, error: 'Unknown command' }, obj.callback);
-                }
-                break;
-        }
-    }
+   
 }
 
 
@@ -271,59 +234,13 @@ async getInstallationId(apiToken, idUser) {
 
 
 
- async FetchApiDetails(username, password) {
-			
-			if (!username || !password) {
-				this.log.error('Username or password is missing.');
-				return;
-			}
-
-			try {
-				// API-Login und Abruf der API-Daten
-				const { apiToken, idUser } = await this.getApiToken(username, password);
-				const  installations = await this.getInstallationId(apiToken, idUser);
-					
-				 if (installations.length > 0) {
-					// Wenn Installationen gefunden wurden, die erste Installation verwenden
-					const { id, name } = installations[0];	
-					 // Speichern der Installationen in der Adapterkonfiguration
-						this.updateConfig({ installations });		
-					
-					
-					
-					
-					
-				// Speichern der API-Daten in der Adapterkonfiguration
-				this.updateConfig({ apiKey: apiToken, installationId: id, installationName: name });
-				
-				this.log.info('installationId ' + id + name);
-				this.log.info('Successfully fetched API token and installation ID.');
-			
-		 }
-			
-			} catch (error) {
-				this.log.error('Error fetching API token or installation ID:', error);
-			}
-	   }
-
-		updateConfig(newConfig) {
-			// Aktualisiere die Adapterkonfiguration
-			this.extendForeignObject('system.adapter.' + this.namespace, {
-				native: {
-					...this.config,
-					...newConfig
-				}
-			});
-		}
 
 
 
 
 
 
-
-
-   // Funktion zur Abfrage der API-Daten und Verarbeitung
+   // Abfrage der API-Daten und Verarbeitung
 startPolling(apiKey, installationIds, interval) {
     const pollData = (installationId, installationName) => {
         const url = `https://vrmapi.victronenergy.com/v2/installations/${installationId}/diagnostics?count=1000`;
@@ -375,7 +292,7 @@ startPolling(apiKey, installationIds, interval) {
     };
 
     // Daten in regelmäßigen Abständen abfragen
-    this.log.info(`Starte API-Abfragen alle ${interval} Sekunden.`);
+    this.log.info(`Starte API-Abfragen alle ${interval / 1000} Sekunden.`);
     this.pollingInterval = setInterval(pollAllData, (interval));
 
     // Initiale API-Abfrage für alle Installationen starten
@@ -392,7 +309,8 @@ processVictronRecords(records,installationName) {
     records.forEach(record => {
         const dbusServiceType = record.dbusServiceType;
         const description = record.description.replace(/[^a-zA-Z0-9-]/g, '_');
-        const basePath = `${installationName}.${dbusServiceType}.${description}`;
+        const rootnode    = installationName.replace(/[^a-zA-Z0-9-]/g, '_');
+		const basePath = `${rootnode}.${dbusServiceType}.${description}`;
         
         this.createDataPoints(record, basePath);
     });
@@ -422,6 +340,8 @@ createDataPoints(record, basePath) {
                     native: {}
                 });
 
+
+			
                 this.setState(dataPointName, { val: value, ack: true });
             }
         }
