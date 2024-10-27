5aba494a9cb5efaf9635e30a1717fec37496831f
Topo <balance240@gmail.com>
2024-10-27 16:36:10 +0000

    add interval per datapoint

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 'use strict';
 
-const utils         		= require('@iobroker/adapter-core');
-const axios         		= require('axios').default;
-const ModbusRTU     		= require('modbus-serial');
-const VRM           		= require('./lib/libvrm.js');
-const TOOLS         		= require('./lib/vrmutils.js');
-const SQLiteDB      		= require('./lib/libdb.js'); // Import SQLiteDB class
-const ModbusClient  		= require('./lib/libmodbus');
-const WebhookClient 		= require('./lib/libWebhook');
-const ModbusDataManager 	= require('./lib/libModbusDataManager.js');
+const utils = require('@iobroker/adapter-core');
+const axios = require('axios').default;
+const ModbusRTU = require('modbus-serial');
+const VRM = require('./lib/libvrm.js');
+const TOOLS = require('./lib/vrmutils.js');
+const SQLiteDB = require('./lib/libdb.js'); // Import SQLiteDB class
+const ModbusClient = require('./lib/libmodbus');
+const WebhookClient = require('./lib/libWebhook');
+const ModbusDataManager = require('./lib/libModbusDataManager.js');
 
 // Hauptadapter-Klasse
 class VictronVrmAdapter extends utils.Adapter {
@@ -20,7 +20,7 @@ class VictronVrmAdapter extends utils.Adapter {
         });
 
         // Initialize the SQLite database
-        this.sqliteDB 			= new SQLiteDB(this, { dbPath: './db/victronDBV02.db' });
+        this.sqliteDB = new SQLiteDB(this, { dbPath: './db/victronDBV02.db' });
 
         // Bind functions
         this.on('ready', this.onReady.bind(this));
@@ -32,28 +32,26 @@ class VictronVrmAdapter extends utils.Adapter {
         this.modbusPollingInterval = null; // Will hold the polling interval ID
 
         // Objects
-        this.vrm 				= new VRM(this); // Create VRM instance
-        this.tools 				= new TOOLS(this); // Create VRM-utils
-        this.modbusClient 		= new ModbusClient(this);
-        this.webhookClient 		= new WebhookClient(this); // New instance of WebhookClient
+        this.vrm = new VRM(this); // Create VRM instance
+        this.tools = new TOOLS(this); // Create VRM-utils
+        this.modbusClient = new ModbusClient(this);
+        this.webhookClient = new WebhookClient(this); // New instance of WebhookClient
 
-		this.modbusDataManager 	= new ModbusDataManager(this); //from xlsx Register update to sqlite3 db!
+        this.modbusDataManager = new ModbusDataManager(this); // For xlsx Register update to sqlite3 db
 
         // Initialize enabledDatapoints
-        this.enabledDatapoints 	= new Set();
+        this.enabledDatapoints = new Set();
 
         // Initialize heartbeat interval
-        this.heartbeatInterval 	= null;
+        this.heartbeatInterval = null;
     }
 
     async onReady() {
         // Log adapter start
         this.log.info('Victron VRM Adapter gestartet.');
 
-		// Prüfe und aktualisiere die XLSX-Datei und die SQLite-Datenbank
+        // Prüfe und aktualisiere die XLSX-Datei und die SQLite-Datenbank
         await this.modbusDataManager.checkAndUpdateXlsxFile();
-
-
 
         // Immediately set the connection state to true
         await this.setState('info.connection', { val: true, ack: true });
@@ -88,70 +86,62 @@ class VictronVrmAdapter extends utils.Adapter {
             return;
         }
 
+        if (!idUser || idUser === 0) {
+            // Fetch idUser from VRM API
+            idUser = await this.vrm.getUserId();
+            this.updateConfig({ idUser: idUser });
+            this.log.warn('idUser geholt: ' + idUser);
+        }
 
-        
-                if (!idUser || idUser === 0) {
-                    // Fetch idUser from VRM API
-                    idUser = await this.vrm.getUserId();
-			
-                    this.updateConfig({ idUser: idUser });
-                    this.log.warn('idUser geholt: ' + idUser);
-                }
-				
-           
-
-
-            // Fetch installations based on BearerToken and idUser
-            this.installations = await this.vrm.getInstallationId(BearerToken, idUser);
-
-            
-
-       
+        // Fetch installations based on BearerToken and idUser
+        this.installations = await this.vrm.getInstallationId(BearerToken, idUser);
 
         // Start the API polling process
         await this.vrm.startPolling(BearerToken, this.installations, interval * 1000);
 
-       // Introduce a 120-second delay before calling getObjectView
-setTimeout(() => {
-    this.getObjectView('system', 'custom', {}, (err, doc) => {
-        if (!err && doc && doc.rows) {
-            doc.rows.forEach(row => {
-                const obj = row.value;
 
-                // Check if the object contains custom settings for this adapter
-                if (obj && obj[this.namespace]) {
-                    const customSettings = obj[this.namespace];
-                    const isEnabled = !!customSettings.enabled;
+        // Introduce a 80-second delay before calling getObjectView
+        setTimeout(() => {
+            this.getObjectView('system', 'custom', {}, (err, doc) => {
+                if (!err && doc && doc.rows) {
+                    doc.rows.forEach(row => {
+                        const obj = row.value;
 
-                    if (isEnabled) {
-                        this.enabledDatapoints.add(row.id);
+                        // Check if the object contains custom settings for this adapter
+                        if (obj && obj[this.namespace]) {
+                            const customSettings = obj[this.namespace];
+                            const isEnabled = !!customSettings.enabled;
 
-                        // Check if Modbus is enabled via the new checkbox (enableModbus)
-                        const isModbusEnabled = !!customSettings.enableModbus;
+                            if (isEnabled) {
+                                this.enabledDatapoints.add(row.id);
 
-                        if (isModbusEnabled) {
-                            // Start the Modbus polling only if the Modbus checkbox is enabled
-                            this.modbusClient.startPollingForDatapoint(row.id, customSettings);
-                            this.log.debug(`${row.id} Modbus-Abfrage aktiviert`);
+                                // Check if Modbus is enabled via the new checkbox (enableModbus)
+                                const isModbusEnabled = !!customSettings.enableModbus;
+                                const modbusInterval = (customSettings.modbusInterval || 60) * 1000;
+
+                                if (isModbusEnabled) {
+                                    // Start the Modbus polling only if the Modbus checkbox is enabled
+                                    this.modbusClient.startPollingForDatapoint(row.id, customSettings, modbusInterval);
+                                    this.log.debug(`${row.id} Modbus-Abfrage aktiviert`);
+                                }
+
+                                // Start Webhook polling if needed
+                                const webhookInterval = (customSettings.webhookInterval || 300) * 1000;
+                                this.webhookClient.startPollingWebhook(row.id, customSettings, webhookInterval);
+
+                                this.log.debug(`${row.id} aktiviert in den Einstellungen`);
+                            }
                         }
-
-                        // Start Webhook polling if needed
-                        this.webhookClient.startPollingWebhook(row.id, customSettings);
-
-                        this.log.debug(`${row.id} aktiviert in den Einstellungen`);
-                    }
+                    });
+                } else {
+                    this.log.error(`Fehler beim Abrufen der Objekte: ${err}`);
                 }
             });
-        } else {
-            this.log.error(`Fehler beim Abrufen der Objekte: ${err}`);
-        }
-    });
-}, 120000); // 120,000 ms = 120 seconds
+        }, 80000); // 120,000 ms = 120 seconds
 
         // Subscribe to all objects
         this.subscribeObjects('*');
     }
-
 
     startHeartbeat() {
         // Send heartbeat every minute
@@ -161,10 +151,6 @@ setTimeout(() => {
                 .catch(err => this.log.error(`Heartbeat error: ${err.message}`));
         }, 60000); // 60,000 ms = 1 minute
     }
-
-
-
-
 
     async onUnload(callback) {
         try {
@@ -192,7 +178,7 @@ setTimeout(() => {
             }
 
             // Stop VRM polling
-            if (this.vrm) { // Corrected from this.vrmClient to this.vrm
+            if (this.vrm) {
                 await this.vrm.stopAllPolling();
                 this.log.info('Alle VRM-Polling-Intervalle wurden gestoppt.');
             }
@@ -211,77 +197,69 @@ setTimeout(() => {
         }
     }
 
-
-
     onObjectChange(id, obj) {
-    if (obj) {
-        // Überprüfen, ob das Objekt zu diesem Adapter gehört
-        if (id.startsWith(`${this.namespace}.`)) {
-            this.log.info(`Objekt ${id} wurde geändert oder hinzugefügt`);
+        if (obj) {
+            if (id.startsWith(`${this.namespace}.`)) {
+                this.log.info(`Objekt ${id} wurde geändert oder hinzugefügt`);
 
-            if (obj.common && obj.common.custom && obj.common.custom[this.namespace]) {
-                const customSettings = obj.common.custom[this.namespace];
-                const isEnabled = !!customSettings.enabled;
+                if (obj.common && obj.common.custom && obj.common.custom[this.namespace]) {
+                    const customSettings = obj.common.custom[this.namespace];
+                    const isEnabled = !!customSettings.enabled;
 
-                this.log.debug(`isEnabled für ${id}: ${isEnabled}`);
-                this.log.debug(`enabledDatapoints.has(${id}): ${this.enabledDatapoints.has(id)}`);
+                    this.log.debug(`isEnabled für ${id}: ${isEnabled}`);
+                    this.log.debug(`enabledDatapoints.has(${id}): ${this.enabledDatapoints.has(id)}`);
 
-                // Webhook-bezogene Aktionen
-                if (customSettings.useWebhook === false) {
-                    this.webhookClient.stopWebhookPolling(id); // Falls erforderlich
-                }
+                    if (customSettings.useWebhook === false) {
+                        this.webhookClient.stopWebhookPolling(id);
+                    }
 
-                if (customSettings.useWebhook === true) {
-                    this.webhookClient.startPollingWebhook(id, customSettings); // Falls erforderlich
-                }
+                    if (customSettings.useWebhook === true) {
+                        const webhookInterval = (customSettings.webhookInterval || 300) * 1000;
+                        this.webhookClient.startPollingWebhook(id, customSettings, webhookInterval);
+                    }
 
-                // Überprüfen, ob das Polling aktiviert ist
-                if (isEnabled) {
-                    // Überprüfen, ob Modbus aktiviert ist
-                    const isModbusEnabled = !!customSettings.enableModbus;
+                    if (isEnabled) {
+                        const isModbusEnabled = !!customSettings.enableModbus;
+                        const modbusInterval = (customSettings.modbusInterval || 60) * 1000;
 
-                    if (!this.enabledDatapoints.has(id)) {
-                        this.enabledDatapoints.add(id);
+                        if (!this.enabledDatapoints.has(id)) {
+                            this.enabledDatapoints.add(id);
 
-                        // Starte die Modbus-Abfrage nur, wenn die Checkbox "enableModbus" aktiviert ist
-                        if (isModbusEnabled) {
-                            this.modbusClient.startPollingForDatapoint(id, customSettings);
-                            this.log.info(`Modbus-Polling für ${id} gestartet.`);
+                            if (isModbusEnabled) {
+                                this.modbusClient.startPollingForDatapoint(id, customSettings, modbusInterval);
+                                this.log.info(`Modbus-Polling für ${id} gestartet.`);
+                            }
+
+                            
+                            
                         }
-
-                        // Webhook-Polling unabhängig vom Modbus starten, falls erforderlich
-                        this.webhookClient.startPollingWebhook(id, customSettings); // Falls erforderlich
-                        this.log.info(`Polling für ${id} gestartet.`);
+                    } else {
+                        this.enabledDatapoints.delete(id);
+                        this.modbusClient.stopPolling(id);
+                        this.webhookClient.stopWebhookPolling(id);
+                        this.log.info(`Polling für ${id} gestoppt.`);
                     }
                 } else {
-                    // Stoppe das Polling, wenn es deaktiviert wurde
+                    // Falls benutzerdefinierte Einstellungen entfernt wurden, stoppe das Polling
+                    this.log.info(`Keine benutzerdefinierten Einstellungen für ${id} vorhanden. Stoppe Polling.`);
                     this.enabledDatapoints.delete(id);
-                    this.modbusClient.stopPolling();
-                    this.webhookClient.stopWebhookPolling(id); // Falls erforderlich
-                    this.log.info(`Polling für ${id} gestoppt.`);
+                    this.modbusClient.stopPolling(id);
+                    this.webhookClient.stopWebhookPolling(id);
                 }
-            } else {
-                // Falls benutzerdefinierte Einstellungen entfernt wurden, stoppe das Polling
-                this.log.info(`Keine benutzerdefinierten Einstellungen für ${id} vorhanden. Stoppe Polling.`);
+            }
+        } else {
+            this.log.info(`Objekt ${id} wurde gelöscht`);
+
+            // Überprüfen, ob das Objekt zu diesem Adapter gehört
+            if (id.startsWith(`${this.namespace}.`)) {
+                // Stoppe das Polling für diesen Datenpunkt
                 this.enabledDatapoints.delete(id);
-                this.modbusClient.stopPolling();
-                this.webhookClient.stopWebhookPolling(id); // Falls erforderlich
+                this.modbusClient.stopPolling(id);
+                this.webhookClient.stopWebhookPolling(id);
+                this.log.info(`Polling für ${id} gestoppt (Objekt gelöscht).`);
             }
         }
-    } else {
-        this.log.info(`Objekt ${id} wurde gelöscht`);
-
-        // Überprüfen, ob das Objekt zu diesem Adapter gehört
-        if (id.startsWith(`${this.namespace}.`)) {
-            // Stoppe das Polling für diesen Datenpunkt
-            this.enabledDatapoints.delete(id);
-            this.modbusClient.stopPolling();
-            this.webhookClient.stopWebhookPolling(id); // Falls erforderlich
-            this.log.info(`Polling für ${id} gestoppt (Objekt gelöscht).`);
-        }
     }
-}
-
 
     async onMessage(obj) {
         // Handle incoming messages if necessary
